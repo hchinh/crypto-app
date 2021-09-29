2b61bbc36fdf840539d511ac82c9bddb2d8b389f
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-09-29 08:34:06 +0700

    Setup layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
-import React from "react";
+import React from 'react';
+import { Switch, Link, Route } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News } from './components';
+import './App.css';
 
 const App = () => {
-  return <div>Future Concurrency</div>;
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/cryptocurrencies/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            Cryptopool
+            <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
